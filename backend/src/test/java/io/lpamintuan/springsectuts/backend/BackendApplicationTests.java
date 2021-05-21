eb120cb60ee27dec10dea4b57056d0886fb40df8
Lyndonn Pamintuan <pamintuan.lyndonn0503@gmail.com>
2021-05-21 22:05:36 +0800

    Initial Commit

---
+++ backend/src/test/java/io/lpamintuan/springsectuts/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.lpamintuan.springsectuts.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
